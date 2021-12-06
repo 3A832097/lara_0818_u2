b20a2d40cf2a3bf9ac0275c72631ae3514a40493
Hsin-Ju Teng <dlulu900821@gmail.com>
2021-12-06 19:55:56 +0800

    practice 3-1 已修改

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-            @extends('layouts.master')
+@extends('layouts.master')
 
-            @section('title','Hello')
+@section('title','Hello')
 
-            @section('content')
-            <h1>This is {{$name}}'s home.</h1>
-            @endsection
-    </body>
-</html>
+@section('content')
+    <h1>This is {{$name}}'s home.</h1>
+@endsection
